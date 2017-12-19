e312520c34109db731595b1a526d7e8ac24b04d1
lockerroom <1053291786@qq.com>
2017-12-19 19:04:12 +0800

    新文件：    src/item/item30/rc_widget.cpp 	新文件：    src/item/item30/rc_widget.h 	新文件：    src/item/item30/rci_ptr.h 	新文件：    src/item/item30/widget.cpp 	新文件：    src/item/item30/widget.h

---
+++ src/item/item30/rc_widget.h
@@ -0,0 +1,20 @@
+/*****************************************************************************/
+/* File Name: rc_widget.h                                                  */
+/* Description: class CRCWidget, like class CMyString         */
+/*                                                                                    */
+/* Author: TyLuo                                                               */
+/* Creation Date: Dece 19, 2017                                      */
+/* Last Revision:                                                              */
+/*****************************************************************************/
+
+#ifndef RC_WIDGET_H_
+#define RC_WIDGET_H_
+
+#include "../../common/common.h"
+
+class CRCWidget
+{
+
+};
+
+#endif
---
+++ src/item/item30/widget.h
@@ -0,0 +1,19 @@
+// class like CStringValue
+#ifndef WIDGET_H_
+#define WIDGET_H_
+
+#include "../../common/common.h"
+
+class CWidget
+{
+public:
+    CWidget(int);
+    CWidget(const CWidget&);
+    ~CWidget();
+    CWidget& operator=(const CWidget&);
+    
+    void doThis();
+    int showWhat() const;
+};
+
+#endif
