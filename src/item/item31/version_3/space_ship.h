e29dcdc4141af1fdf32093a759e2a30bf4219cc8
lockerroom <1053291786@qq.com>
2017-12-27 22:16:07 +0800

    modified:   version_3/asteroid.cpp 	modified:   version_3/asteroid.h 	new file:   version_3/collision_process.cpp 	new file:   version_3/collision_process.h 	modified:   version_3/space_ship.cpp 	modified:   version_3/space_ship.h 	modified:   version_3/space_station.cpp 	modified:   version_3/space_station.h

--- src/item/item31/version_3/space_ship.h
+++ src/item/item31/version_3/space_ship.h
@@ -4,28 +4,10 @@
 #include "../../../common/common.h"
 #include "game_object.h"
 
-class CSpaceShip;
-class CSpaceStation;
-class CAsteroid;
-
 class CSpaceShip : public CGameObject
 {
 public:
     CSpaceShip();
-
-public:
-    virtual void collide(CGameObject&);
-    virtual void hitSpaceShip(const CGameObject&);
-    virtual void hitSpaceStation(const CGameObject&);
-    virtual void hitAsteroid(const CGameObject&);
-
-private:
-    typedef void (CSpaceShip::*HitFunctionPtr)(const CGameObject&);
-    typedef std::map<std::string, HitFunctionPtr> HitMap;
-
-    HitMap m_hit_map;
-    void initializeCollisionMap();
-    HitFunctionPtr lookup(const CGameObject&);
 };
 
 #endif
