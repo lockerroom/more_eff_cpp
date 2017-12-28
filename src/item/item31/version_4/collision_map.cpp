31f92450caaf939d44cc7f2e5c2e1c8e658efca8
TyLuo <tyluo@localhost.localdomain>
2017-12-28 19:08:11 +0800

    修改：      src/item/item31/version_4/collision_map.cpp 	修改：      src/item/item31/version_4/collision_map.h

---
+++ src/item/item31/version_4/collision_map.cpp
@@ -0,0 +1,74 @@
+#include "collision_map.h"
+
+CCollisionMap::CCollisionMap m_instance;
+
+CCollisionMap::CCollisionMap()
+{
+
+}
+
+CCollisionMap::CCollisionMap(const CCollisionMap& rhs)
+{
+
+}
+
+CCollisionMap& CCollisionMap::instance()
+{
+    return m_instance;
+}
+
+void CCollisionMap::add_entry(const CGameObject& obj1, const CGameObject& obj2, HitFunctionPtr phf, bool symmetric/* = true */)
+{
+    add_entry(typeid(obj1).name(),
+                    typeid(obj2).name(),
+                    phf,
+                    symmetric
+                    );
+}
+
+void CCollisionMap::delete_entry(const CGameObject& obj1, const CGameObject& obj2)
+{
+    delete_entry(typeid(obj1).name(), typeid(obj2).name());
+}
+
+HitFunctionPtr CCollisionMap::lookup(const CGameObject& obj1, const CGameObject& obj2) const
+{
+    return lookup(typeid(obj1).name(), typeid(obj2).name());
+}
+
+void CCollisionMap::add_entry(const std::string& type1, const std::string& type2, HitFunctionPtr phf, bool symmetric/* = true */)
+{
+    KeyType key = make_key(type1, type2);
+    m_hit_map.insert(std::make_pair(key, phf));
+}
+
+void CCollisionMap::delete_entry(const std::string& type1, const std::string& type2)
+{
+    KeyType key = make_key(type1, type2);
+
+    HitMap::iterator it = m_hit_map.find(key);
+    if (it != m_hit_map.end())
+    {
+        m_hit_map.erase(it);
+    }
+}
+
+HitFunctionPtr CCollisionMap::lookup(const std::string& obj1_type, const std::string& obj2_type) const
+{
+    KeyType key = make_key(obj1_type, obj2_type);
+
+    HitMap::const_iterator cit = m_hit_map.find(key);
+    if (cit != m_hit_map.end())
+    {
+        return (*cit).second;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+KeyType CCollisionMap::make_key(const std::string& str1, const std::string& str2)
+{
+    return std::make_pair(str1, str2);
+}
--- src/item/item31/version_4/collision_map.h
+++ src/item/item31/version_4/collision_map.h
@@ -13,18 +13,26 @@ private:
 
 public:
     typedef void (*HitFunctionPtr)(const CGameObject&, const CGameObject&);
+    typedef std::pair<std::string, std::string> KeyType;
+    typedef std::map<KeyType, HitFunctionPtr> HitMap;
 
 public:
+    static CCollisionMap& instance();
+
+    void add_entry(const CGameObject& obj1, const CGameObject& obj2, HitFunctionPtr phf, bool symmetric = true);
+    void delete_entry(const CGameObject& obj1, const CGameObject& obj2);
+    HitFunctionPtr lookup(const CGameObject&, const CGameObject&) const ;
+    
+private:
     void add_entry(const std::string& type1, const std::string& type2, HitFunctionPtr phf, bool symmetric = true);
     void delete_entry(const std::string& type1, const std::string& type2);
+    HitFunctionPtr lookup(const std::string&, const std::string&) const ;
 
-    HitFunctionPtr lookup(const CGameObject&, const CGameObject&);
-    HitFunctionPtr lookup(const std::string&, const std::string&);
-
-    static CCollisionMap& instance();
+    KeyType make_key(const std::string&, const std::string&);
 
 private:
     static CCollisionMap m_instance;
+    HitMap m_hit_map;    
 }; 
 
 #endif
